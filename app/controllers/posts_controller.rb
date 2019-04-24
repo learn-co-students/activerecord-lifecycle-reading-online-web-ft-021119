ebdc849b7a10fe284ecf74c0927ea30f26cd0655
Lizzie <lchan217@gmail.com>
2019-04-24 10:55:11 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,8 +19,11 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params.require(:post))
+		if @post.update(params.require(:post).permit(:title, :description))
 	  redirect_to post_path(@post)
+		else
+			render :edit
+		end
 	end
 
 	def edit
